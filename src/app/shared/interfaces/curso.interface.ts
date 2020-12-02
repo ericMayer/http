1cdc73b28d8138ecad2e4aad81746b2e792c36b7
Eric Araujo <eric.rocha@softwillians.com.br>
2020-12-01 22:36:21 -0300

    Criado arquivo json de cursos para servir como API usando o json-server.

---
+++ src/app/shared/interfaces/curso.interface.ts
@@ -0,0 +1,10 @@
+import { Base } from './base.interface';
+
+export interface Curso extends Base {
+  nome?: string;
+  descricao?: string;
+  cargaHoraria?: number;
+  preco?: number;
+  cursosOpcionais?: Base[];
+  preRequisitos?: Base[];
+}
